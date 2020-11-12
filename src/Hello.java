2a97d9cc09490bbdb2571f87a644f34a1b855e71
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2020-11-11 22:43:08 -0600

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    // more stuff
+    // Mac user and windows user are happy to see commit dialog finally
+
+    // I have already published to github.com
+    // now I am ready to make more commits
+    // but do I need to share again ????
+    // ABSOLUTELY NO
+    // IT'S TIME TO COMMIT AND JUST PUSH !!!!!
+
+}
