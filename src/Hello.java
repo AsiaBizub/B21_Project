5795282c40975c05f5257cac98b0b4ed89b5ac74
AsiaBizub <74011714+AsiaBizub@users.noreply.github.com>
2020-11-12 14:11:26 -0600

    Initial commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,12 +1,16 @@
 public class Hello {
+    public static void main(String[] args) {
 
-    // more stuff
-    // Mac user and windows user are happy to see commit dialog finally
 
-    // I have already published to github.com
-    // now I am ready to make more commits
-    // but do I need to share again ????
-    // ABSOLUTELY NO
-    // IT'S TIME TO COMMIT AND JUST PUSH !!!!!
+        // more stuff
+        // Mac user and windows user are happy to see commit dialog finally
 
+        // I have already published to github.com
+        // now I am ready to make more commits
+        // but do I need to share again ????
+        // ABSOLUTELY NO
+        // IT'S TIME TO COMMIT AND JUST PUSH !!!!!
+
+    }
 }
+
